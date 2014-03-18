feb0bbebdc6e322cf23d3ce11d9125fb8a88941d
Marcellus Siegburg <msiegbur@imn.htwk-leipzig.de>
2014-03-18 18:23:20 +0100

    Fixed that testcases were ignoring unless.

--- modules/test/manifests/testcase.pp
+++ modules/test/manifests/testcase.pp
@@ -1,14 +1,16 @@
 ###  (c) Marcellus Siegburg, 2013, License: GPL
-define test::testcase($command, $success = '', $failure = '', $unless = undef) {
+define test::testcase($command, $success = '', $failure = '', $unless = []) {
   exec { "${title} pass: ${success}":
     command   => "echo '${title} pass: ${success}'",
     onlyif    => $command,
     logoutput => true,
+    unless    => $unless,
   }
 
   exec { "${title} fail: ${failure}":
     command   => "echo '${title} pass: ${failure}'",
     onlyif    => "bash -c 't=\"bash -c ${command}\"; test $? -ne 0'",
     logoutput => true,
+    unless    => $unless,
   }
 }
