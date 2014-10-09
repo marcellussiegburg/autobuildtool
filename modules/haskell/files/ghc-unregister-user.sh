9221ede8857b15eae8a8310bed27b2940008f63f
Marcellus Siegburg <msiegbur@imn.htwk-leipzig.de>
2014-10-09 16:18:23 +0200

    Improved unregistering of ghc-pkg user packages.

--- modules/haskell/files/ghc-unregister-user.sh
+++ modules/haskell/files/ghc-unregister-user.sh
@@ -3,8 +3,9 @@
 
 for ((i=0; i<$(ghc-pkg list --user | wc -l); i++))
 do
-    for package in $(ghc-pkg list --user)
+    for package in $(ghc-pkg list --user | sed -e "s/(\(.*\))/\1/g" -e "/\/.*/d" -e "/no packages/d")
     do
 	ghc-pkg unregister $package
     done
 done
+exit 0
