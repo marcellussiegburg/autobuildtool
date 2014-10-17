009e7c73979b829f66c7e514064bed975e2e075d
Marcellus Siegburg <msiegbur@imn.htwk-leipzig.de>
2014-10-17 23:30:29 +0200

    Removed redundant check in ghc unregister.

--- modules/haskell/files/ghc-unregister-user.sh
+++ modules/haskell/files/ghc-unregister-user.sh
@@ -5,7 +5,7 @@ for ((i=0; i<$(ghc-pkg list --user | wc -l); i++))
 do
     for package in $(ghc-pkg list --user | sed -e "s/(\(.*\))/\1/g" -e "/\/.*/d" -e "/no packages/d")
     do
-	ghc-pkg unregister $(echo $package | sed -e "s/(\(.*\))/\1/g" -e "/\/.*/d")
+	ghc-pkg unregister $package
     done
 done
 exit 0
